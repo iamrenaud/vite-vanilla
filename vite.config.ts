f5b2856a270f467a2b52296ffae1dce6c5075993
Renaud <renaudwoanapro@gmail.com>
2025-09-10 19:26:56 +0800

    added tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
     server: {
         host: true,
         allowedHosts: true
-    }
+    },
+    plugins: [
+        tailwindcss(),
+    ],
 })
